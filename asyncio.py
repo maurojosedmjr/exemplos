131842d2057fb09ccee93ca4eb5e035f6d8e75ac
Mauro Martins Jr <maurojosedmjr@gmail.com>
2021-01-21 13:14:04 -0300

    Create asyncio.py

---
+++ asyncio.py
@@ -0,0 +1,29 @@
+import asyncio
+
+# exemplo besta de asyncio
+
+def add(n):
+    return n[0] + n[1]
+
+async def exec_callable(loop: asyncio.get_event_loop, map_exec: Dict):
+    result = []
+    for value in map_exec.values():
+        result.append(loop.run_in_executor(None, value[0], value[1]))
+        print(result)
+    return result
+
+map_execs = {
+    "a": (add, (1, 3)),
+    "b": (add, (8, 1)),
+    "c": (add, (4, 2)),
+    "d": (add, (1, 1)),
+}
+
+loop = asyncio.get_event_loop()
+
+result = loop.run_until_complete(
+    exec_callable(loop, map_exec=map_execs)
+)
+
+for r in result:
+    print(r.result())
