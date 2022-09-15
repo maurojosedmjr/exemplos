747a5c4c02c5176e040e5f89a9da85a2d7bcea2b
Mauro Martins Jr <maurojosedmjr@gmail.com>
2022-09-15 16:21:52 -0300

    Create multiprocessing_pool.py

---
+++ multiprocessing_pool.py
@@ -0,0 +1,48 @@
+from multiprocessing.pool import ThreadPool
+from time import sleep
+from datetime import datetime
+from typing import List
+
+RRANGE: range = range(10, 0, -1)
+
+
+def wait(seconds: int = 5) -> str:
+    print(f"Waiting for {seconds} seconds!")
+    sleep(seconds)
+    print(f"Waited for {seconds} seconds!")
+    return f"Waited for {seconds} seconds!"
+
+
+def do_it_with_threads() -> List[str]:
+    pool = ThreadPool(4)
+    results: List[str] = []
+    for i in RRANGE:
+        results.append(pool.apply_async(wait, (i,)))
+
+    return [r.get() for r in results]
+
+
+def do_it_without_threads() -> None:
+    results: List[str] = []
+    for i in RRANGE:
+        results.append(wait(i))
+    return results
+
+
+def main():
+    print("Starting first step")
+    st_with_threds: datetime = datetime.now()
+    result_with_threads = do_it_with_threads()
+    print(f"Elapsed {datetime.now() - st_with_threds}")
+
+    print("Starting second step")
+    st_without_threds: datetime = datetime.now()
+    result_without_threads = do_it_without_threads()
+    print(f"Elapsed {datetime.now() - st_without_threds}")
+
+    print(f"Result with Threads: '{result_with_threads}'.")
+    print(f"Result with Threads: '{result_without_threads}'.")
+
+
+if __name__ == "__main__":
+    main()
