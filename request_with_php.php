cf6a77f96797e63382ac3c243e1bf598164cd178
Mauro Martins Jr <maurojosedmjr@gmail.com>
2022-10-10 12:42:21 -0300

    Update request_with_php.php

--- request_with_php.php
+++ request_with_php.php
@@ -1,7 +1,7 @@
 <?php
 
 function callAuthFunction($url, $email, $password) {
-	$ch = curl_init();
+    $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     $arrayAuth = array('email' => $email, 'senha' => $password);
     $authDict = json_encode($arrayAuth);
@@ -21,14 +21,10 @@ $senha = '';
 
 $result = callAuthFunction($urllogin, $email, $senha);
 
-// echo $result;
-
 $urlRequest = '';
 
-// Create a new cURL resource
 $ch = curl_init($urlRequest);
 
-// Setup request to send json via POST
 $data = array(
     'key1' => 'Valor 1',
     'key2' => 'valor 2'
@@ -36,25 +32,17 @@ $data = array(
 
 $payload = json_encode($data);
 
-echo $payload;
-echo "\n\n";
-
-// Attach encoded JSON string to the POST fields
 curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
 
-// Set the content type to application/json
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json', "Authorization: Bearer {$result}"));
 
-// Return response instead of outputting
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-// Execute the POST request
 $result = curl_exec($ch);
 
 echo $result;
 echo "\n\n";
 
-// Close cURL resource
 curl_close($ch);
 
 ?>
